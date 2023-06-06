906a68881e826603ed5a74b7b66fbbd317892ca2
LakshayBhati <99648962+LakshayBhati@users.noreply.github.com>
2023-06-06 22:03:15 +0530

    Update dataBase.js

--- config/dataBase.js
+++ config/dataBase.js
@@ -1,9 +1,18 @@
 import mongoose from "mongoose"
 
-const connectDB = async() => {
-const { connection } =  await mongoose.connect(process.env.MONGO_URI)
-
-console.log(`database is connectd with ${connection.host}`)
+const connectDB = async () => {
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URI ,{
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+            
+        })
+        console.log(conn.connection.host)
+    }
+    catch(error){
+        console.log(`Error: ${error.message}`),
+        process.exit(1)
+    }
 }
 
 export default connectDB

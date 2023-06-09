49d529f40354f4b15204daeb0ae5e2aa5725685b
LakshayBhati <99648962+LakshayBhati@users.noreply.github.com>
2023-06-09 21:25:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,9 +21,9 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    secure: process.env.NODE_ENV === "production",
+    secure: process.env.NODE_ENV === production,
     httpOnly: true,
-    sameSite: process.env.NODE_ENV === "production" ? None : false,
+    sameSite: process.env.NODE_ENV === production ? None : false,
   },
 }));
 

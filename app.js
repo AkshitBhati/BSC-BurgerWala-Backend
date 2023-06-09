a923e4bfe7fb3576584088328b325a5a09456f08
LakshayBhati <99648962+LakshayBhati@users.noreply.github.com>
2023-06-09 21:15:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,16 +17,16 @@ dotenv.config({
 
 //Using MiddleWares
 app.use(session({
-    secret:process.env.SESSION_SECRET,
-    resave:false,
-    saveUninitialized:false,
-    cookie:{
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
     secure: process.env.NODE_ENV === "production",
-  httpOnly: true,
-  sameSite: process.env.NODE_ENV === "production" ? "None" : false,
+    httpOnly: true,
+    sameSite: process.env.NODE_ENV === "production" ? "None" : false,
+  },
+}));
 
-    }
-}))
 
 app.use(passport.authenticate("session"))
 app.use(passport.initialize())
